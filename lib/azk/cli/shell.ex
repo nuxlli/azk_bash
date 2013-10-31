5a8295fde35ad289e5a8d543cb23019b140e61b2
Renato Albano <renatoalbano@gmail.com>
2013-10-31 21:26:43 -0200

    io help commando

--- lib/azk/cli/shell.ex
+++ lib/azk/cli/shell.ex
@@ -1,4 +1,6 @@
 defmodule Azk.Shell do
   # TODO: Improvement to return the output and return code
   defdelegate cmd(command, func), to: Mix.Shell
+
+  defdelegate info(message), to: Mix.Shell.IO
 end
