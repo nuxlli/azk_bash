700916c28b511b800ce1c86640ed1fe62d99df21
Éverton Ribeiro <nuxlli@gmail.com>
2013-11-07 18:57:17 -0200

    Adding short description to info and exec commands.

--- lib/azk/cli/commands/info.ex
+++ lib/azk/cli/commands/info.ex
@@ -1,6 +1,8 @@
 defmodule Azk.Cli.Commands.Info do
   use Azk.Cli.Command
 
+  @shortdoc "Show information about azk application"
+
   def run(app_path, _argv) do
     app = AzkApp.new(app_path).load!
     IO.inspect(app)
