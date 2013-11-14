e185a3ce861daca446041eaa68aafb9fc454232c
Éverton Ribeiro <nuxlli@gmail.com>
2013-11-14 12:39:12 -0200

    Removing elixir version.

--- lib/azk/cli/shell.ex
+++
@@ -1,6 +0,0 @@
-defmodule Azk.Shell do
-  # TODO: Improvement to return the output and return code
-  defdelegate cmd(command, func), to: Mix.Shell
-
-  defdelegate info(message), to: Mix.Shell.IO
-end
